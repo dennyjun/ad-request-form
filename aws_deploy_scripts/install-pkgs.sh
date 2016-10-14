aa804a7c61330b69127048dca45e32cc4afb6458
Denny Jun <denny.6a756e@gmail.com>
2016-10-14 00:54:13 -0700

    [config] install build tools for amazons linux ec2 instance

--- aws_deploy_scripts/install-pkgs.sh
+++ aws_deploy_scripts/install-pkgs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/ad-request-form
-curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
-yum -y install nodejs
+curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash -
+sudo yum -y install nodejs
+sudo yum install gcc-c++ make
 npm install
 node_modules/pm2/bin/pm2 install coffeescript
