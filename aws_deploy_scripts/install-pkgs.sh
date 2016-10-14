740964c1b246b20981ada094c7d4f4643636faed
Denny Jun <denny.6a756e@gmail.com>
2016-10-13 18:59:46 -0700

    [config] remove unused scripts

--- aws_deploy_scripts/install-pkgs.sh
+++ aws_deploy_scripts/install-pkgs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/ad-request-form
+curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash -
+sudo yum -y install nodejs
 npm install
 node_modules/pm2/bin/pm2 install coffeescript
--- aws_deploy_scripts/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/ad-request-form
-curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash -
-sudo yum -y install nodejs
